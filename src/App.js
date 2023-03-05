eb46f22896f70e65b2b68f2b5402a0cbd00883be
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-03-05 00:12:41 -0300

    Building the structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as S from "./styled"
+
+function App() {
+  return (
+    <S.App>
+      <img src="https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/Github-Dark.svg" alt="" />
+    </S.App>
+  );
+}
+
+export default App;
